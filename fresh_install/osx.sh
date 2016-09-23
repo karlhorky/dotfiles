29d3d95696c5da7ae505a2b3183d3f701262dbf7
Karl Horky <karl.horky@gmail.com>
2016-09-23 14:13:46 +0200

    Fix fast key repeat for macOS Sierra

--- fresh_install/osx.sh
+++ fresh_install/osx.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-info "Configuring OS X System settings..."
+info "Configuring macOS System settings..."
 
 # Disable press-and-hold for keys in favor of key repeat
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 0
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
 
 # Increase window resize speed for Cocoa applications
 # Affects showing and hiding sheets, resizing preference windows, zooming windows
