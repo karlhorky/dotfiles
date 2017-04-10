4401410f46d346d1bb58be4c37d3e36bfc1425ab
Karl Horky <karl.horky@gmail.com>
2017-04-10 11:35:42 +0200

    Add Atom packages

--- fresh_install/brew_install.sh
+++ fresh_install/brew_install.sh
@@ -27,11 +27,25 @@ main() {
   
   execute "brew cask install google-chrome" "brew cask install google-chrome"
   execute "brew cask install hyper" "brew cask install hyper"
-  execute "brew cask install atom" "brew cask install atom"
   execute "brew cask install slack" "brew cask install slack"
   execute "brew cask install steelseries-exactmouse-tool" "brew cask install steelseries-exactmouse-tool"
   execute "brew cask install magicprefs" "brew cask install magicprefs"
   execute "brew cask install flycut" "brew cask install flycut"
+
+  # Install Atom and packages
+  execute "brew cask install atom" "brew cask install atom"
+  execute "apm install atom-alignment" "apm install atom-alignment"
+  execute "apm install easy-motion-redux" "apm install easy-motion-redux"
+  execute "apm install editorconfig" "apm install editorconfig"
+  execute "apm install highlight-line" "apm install highlight-line"
+  execute "apm install language-groovy" "apm install language-groovy"
+  execute "apm install language-reason" "apm install language-reason"
+  execute "apm install linter" "apm install linter"
+  execute "apm install linter-flow" "apm install linter-flow"
+  execute "apm install n-panes" "apm install n-panes"
+  execute "apm install prettier-atom" "apm install prettier-atom"
+  execute "apm install save-session" "apm install save-session"
+  execute "apm install vim-mode-plus" "apm install vim-mode-plus"
 
   # Install Mac App Store software
   execute "brew install mas" "brew install mas"
