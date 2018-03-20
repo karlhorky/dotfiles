8f343f0ff2b7ff2cb0731eaa1df07725981753af
Karl Horky <karl.horky@gmail.com>
2018-03-20 13:30:14 +0100

    Add user script to strip UTM parameters

---
+++ utmstrip.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name          UTM param stripper
+// @author        Paul Irish
+// @namespace     http://github.com/paulirish
+// @version       1.2
+// @description   Drop the UTM params from a URL when the page loads.
+// @extra         Cuz you know they're all ugly n shit.
+// @include       http*://*
+// ==/UserScript==
+
+// You can install this user script by downloading this script,
+// going to about: extensions, turning on developer mode and dragging and dropping
+// this file onto the window. That'll install it.
+
+// lastly, if your site / marketing funnel uses these tracking tokens. you can clean up your users URLs
+// look at the comments below on correct installation to integrate with __gaq.push
+
+if (/utm_/.test(location.search) && window.history.replaceState) {
+  // thx @cowboy for the revised hash param magic.
+  var oldUrl = location.href;
+  var newUrl = oldUrl.replace(/\?([^#]*)/, function(_, search) {
+    search = search
+      .split("&")
+      .map(function(v) {
+        return !/^utm_/.test(v) && v;
+      })
+      .filter(Boolean)
+      .join("&"); // omg filter(Boolean) so dope.
+    return search ? "?" + search : "";
+  });
+
+  if (newUrl != oldUrl) {
+    window.history.replaceState({}, "", newUrl);
+  }
+}
