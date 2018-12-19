80e84e6f296a528969800a4780a895ad8cb6cf59
Karl Horky <karl.horky@gmail.com>
2018-12-19 16:07:29 +0100

    Add Now Desktop

--- fresh_install/brew_install.sh
+++ fresh_install/brew_install.sh
@@ -75,6 +75,8 @@ main() {
   execute "brew cask install iterm2" "brew cask install iterm2"
   # Install Adobe Acrobat Reader DC
   execute "brew cask install adobe-acrobat-reader" "brew cask install adobe-acrobat-reader"
+  # Install Now Desktop
+  execute "brew cask install now" "brew cask install now"
 
   # Add drivers tap and install SteelSeries ExactMouse to disable macOS mouse acceleration
   execute "brew tap caskroom/drivers" "brew tap caskroom/drivers"
