2e92c86b63f329cc83488b854a5d67e2310ec7e1
Karl Horky <karl.horky@gmail.com>
2020-06-11 11:58:44 +0200

    Fix filename

--- cron/setup-trash-downloads-screenshots.sh
+++ cron/setup-trash-downloads-screenshots.sh
@@ -2,7 +2,8 @@
 
 main() {
   local script_path=/usr/local/trash-downloads-screenshots-older-than-a-week.sh
-  echo 'find ~/Downloads -type f -mtime +7 -name "Screen Shot*" -exec mv {} ~/.Trash \;' > "$script_path"
+  local script_content='find ~/Downloads -type f -mtime +7 -name "Screen Shot*" -exec mv {} ~/.Trash \;'
+  echo "$script_content" > "$script_path"
   # This may fail if there's no crontab yet, you may need to create one with:
   # crontab -e
   crontab -u k -l | { cat; echo "0 10 * * * sh $script_path"; } | crontab -u k -
