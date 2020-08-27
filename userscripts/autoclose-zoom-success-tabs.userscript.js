a2ba61c125d853abeb4ea089635da70268c86275
Karl Horky <karl.horky@gmail.com>
2020-08-27 10:45:50 +0200

    Add Zoom autoclose script

---
+++ userscripts/autoclose-zoom-success-tabs.userscript.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Autoclose Zoom Success Tabs
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Automatically close Zoom tabs after success à la https://github.com/thesephist/clozoom/blob/master/background.js
+// @author       Karl Horky
+// @match        https://*.zoom.us/j/*
+// @grant        window.close
+// ==/UserScript==
+
+(function() {
+  'use strict';
+  const url = location.href;
+
+  // https://us02web.zoom.us/j/6830992169#success
+  const isZoomInviteUrl = url.match(/https:\/\/\S+\.zoom.us\/j\/.+/);
+  const isSuccess = url.endsWith('#success');
+
+  if (isZoomInviteUrl && isSuccess) {
+    setTimeout(() => window.close(), 2000);
+  }
+})();
