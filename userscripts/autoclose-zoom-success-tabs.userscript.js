dc5d90566d8aed4e87ee063348d086f4ce68f2fc
Karl Horky <karl.horky@gmail.com>
2020-11-06 09:49:22 +0100

    Fix URL scope

--- userscripts/autoclose-zoom-success-tabs.userscript.js
+++ userscripts/autoclose-zoom-success-tabs.userscript.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Autoclose Zoom Success Tabs
 // @namespace    https://www.karlhorky.com/
-// @version      1.0.1
+// @version      1.0.2
 // @description  Automatically close Zoom tabs after success à la https://github.com/thesephist/clozoom/blob/master/background.js
 // @author       Karl Horky
-// @match        https://*.zoom.us/j/*
+// @match        https://*.zoom.us/postattendee/*
 // @grant        window.close
 // ==/UserScript==
 
