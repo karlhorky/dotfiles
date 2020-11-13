bc096939bbc93c7b4e8b6a10de981baf4aee3dd3
Karl Horky <karl.horky@gmail.com>
2020-11-13 10:42:58 +0100

    Add back old URL format too

--- userscripts/autoclose-zoom-success-tabs.userscript.js
+++ userscripts/autoclose-zoom-success-tabs.userscript.js
@@ -1,17 +1,21 @@
 // ==UserScript==
 // @name         Autoclose Zoom Success Tabs
 // @namespace    https://www.karlhorky.com/
-// @version      1.0.3
+// @version      1.0.4
 // @description  Automatically close Zoom tabs after success à la https://github.com/thesephist/clozoom/blob/master/background.js
 // @author       Karl Horky
-// @match        https://*.zoom.us/postattendee*
+// @match        https://*.zoom.us/*
 // @grant        window.close
 // ==/UserScript==
 
 (function() {
   'use strict';
-  // https://us02web.zoom.us/postattendee?id=8
-  if (/^https:\/\/\S+\.zoom.us\/postattendee\?id=/.test(location.href)) {
+  if (
+    // https://us02web.zoom.us/j/6830992169#success
+    /^https:\/\/\S+\.zoom.us\/j\/.+#success$/.test(location.href) ||
+    // https://us02web.zoom.us/postattendee?id=8
+    /^https:\/\/\S+\.zoom.us\/postattendee\?id=/.test(location.href)
+  ) {
     setTimeout(() => window.close(), 2000);
   }
 })();
