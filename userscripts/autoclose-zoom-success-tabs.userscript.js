37be067854a12271c97c0a78190a4f66ab937da9
Karl Horky <karl.horky@gmail.com>
2020-11-23 09:14:59 +0100

    Fix URL pattern

--- userscripts/autoclose-zoom-success-tabs.userscript.js
+++ userscripts/autoclose-zoom-success-tabs.userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Autoclose Zoom Success Tabs
 // @namespace    https://www.karlhorky.com/
-// @version      1.0.4
+// @version      1.0.5
 // @description  Automatically close Zoom tabs after success à la https://github.com/thesephist/clozoom/blob/master/background.js
 // @author       Karl Horky
 // @match        https://*.zoom.us/*
@@ -14,7 +14,8 @@
     // https://us02web.zoom.us/j/6830992169#success
     /^https:\/\/\S+\.zoom.us\/j\/.+#success$/.test(location.href) ||
     // https://us02web.zoom.us/postattendee?id=8
-    /^https:\/\/\S+\.zoom.us\/postattendee\?id=/.test(location.href)
+    // https://us02web.zoom.us/postattendee
+    /^https:\/\/\S+\.zoom.us\/postattendee/.test(location.href)
   ) {
     setTimeout(() => window.close(), 2000);
   }
