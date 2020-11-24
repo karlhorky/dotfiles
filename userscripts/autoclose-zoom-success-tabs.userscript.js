39e9d14ce4cb363739c9443887f869932128c93b
Karl Horky <karl.horky@gmail.com>
2020-11-24 10:36:01 +0100

    Fix for hash change to #success after load

--- userscripts/autoclose-zoom-success-tabs.userscript.js
+++ userscripts/autoclose-zoom-success-tabs.userscript.js
@@ -1,22 +1,24 @@
 // ==UserScript==
 // @name         Autoclose Zoom Success Tabs
 // @namespace    https://www.karlhorky.com/
-// @version      1.0.5
+// @version      1.0.6
 // @description  Automatically close Zoom tabs after success à la https://github.com/thesephist/clozoom/blob/master/background.js
 // @author       Karl Horky
 // @match        https://*.zoom.us/*
 // @grant        window.close
 // ==/UserScript==
 
-(function() {
+(function () {
   'use strict';
-  if (
-    // https://us02web.zoom.us/j/6830992169#success
-    /^https:\/\/\S+\.zoom.us\/j\/.+#success$/.test(location.href) ||
-    // https://us02web.zoom.us/postattendee?id=8
-    // https://us02web.zoom.us/postattendee
-    /^https:\/\/\S+\.zoom.us\/postattendee/.test(location.href)
-  ) {
-    setTimeout(() => window.close(), 2000);
-  }
+  setTimeout(() => {
+    if (
+      // https://us02web.zoom.us/j/6830992169#success
+      /^https:\/\/\S+\.zoom.us\/j\/.+#success$/.test(location.href) ||
+      // https://us02web.zoom.us/postattendee?id=8
+      // https://us02web.zoom.us/postattendee
+      /^https:\/\/\S+\.zoom.us\/postattendee/.test(location.href)
+    ) {
+      window.close();
+    }
+  }, 10000);
 })();
