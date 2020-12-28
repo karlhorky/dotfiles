1c5fc9c6aa6079d91c091fc61811bd9c509873d3
Karl Horky <karl.horky@gmail.com>
2020-12-28 18:27:29 +0100

    Append styles to body, match elements explicitly

--- userscripts/always-allow-selection.user.js
+++ userscripts/always-allow-selection.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Always Allow Selection
 // @namespace    https://www.karlhorky.com/
-// @version      1.0
+// @version      2.0
 // @description  Disable user-select: none on all elements
 // @author       Karl Horky
 // @match        https://*/*
@@ -9,9 +9,11 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
   'use strict';
   const styleEl = document.createElement('style');
-  document.head.appendChild(styleEl);
-  styleEl.sheet.insertRule('* { user-select: auto !important; }');
+  document.body.appendChild(styleEl);
+  styleEl.sheet.insertRule(
+    '*, a, abbr, address, area, article, aside, audio, b, base, bdi, bdo, blockquote, body, br, button, canvas, caption, cite, code, col, colgroup, data, datalist, dd, del, details, dfn, dialog, div, dl, dt, em, embed, fieldset, figcaption, figure, footer, form, h1, h2, h3, h4, h5, h6, head, header, hgroup, hr, html, i, iframe, img, input, ins, kbd, label, legend, li, link, main, map, mark, math, menu, menuitem, meta, meter, nav, noscript, object, ol, optgroup, option, output, p, param, picture, pre, progress, q, rb, rp, rt, rtc, ruby, s, samp, script, section, select, slot, small, source, span, strong, style, sub, summary, sup, svg, table, tbody, td, template, textarea, tfoot, th, thead, time, title, tr, track, u, ul, var, video, wbr { user-select: auto !important; }',
+  );
 })();
