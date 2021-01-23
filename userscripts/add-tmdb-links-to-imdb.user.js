d7dde2cde999c26dcc5b8ab101ec1a8c85f2f82b
Karl Horky <karl.horky@gmail.com>
2021-01-23 12:37:35 +0100

    Test version bump

--- userscripts/add-tmdb-links-to-imdb.user.js
+++ userscripts/add-tmdb-links-to-imdb.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Add TMDb links to IMDb
 // @description  Add links to themoviedb.org on IMDb pages
-// @version      1.0.0
+// @version      1.0.1
 // @author       Karl Horky
 // @namespace    https://www.karlhorky.com/
 // @match        https://www.imdb.com/title/*
