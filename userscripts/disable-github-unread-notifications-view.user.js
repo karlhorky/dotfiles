df811fb2a7e8b06b26082136034ed5c8a2816d35
Karl Horky <karl.horky@gmail.com>
2021-02-11 22:17:48 +0100

    Click on button to set preference

--- userscripts/disable-github-unread-notifications-view.user.js
+++ userscripts/disable-github-unread-notifications-view.user.js
@@ -1,11 +1,15 @@
 // ==UserScript==
 // @name         Disable GitHub Unread Notifications view
 // @description  Redirect away from Notifications "Unread" view
-// @version      1.0.0
+// @version      1.1.0
 // @author       Karl Horky
 // @namespace    https://www.karlhorky.com/
 // @match        https://github.com/notifications?query=is%3Aunread
 // @grant        none
 // ==/UserScript==
 
-window.location.href = 'https://github.com/notifications?query=';
+document
+  .querySelector(
+    '.BtnGroup > form[action="/notifications/beta/set_preferred_inbox_query"]:first-child > button'
+  )
+  .click();
