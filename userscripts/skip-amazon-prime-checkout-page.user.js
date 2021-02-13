2d837f323a914bddd3dcd65de5fe223265ae14ac
Karl Horky <karl.horky@gmail.com>
2021-02-13 16:58:13 +0100

    Create skip-amazon-prime-checkout-page.user.js

---
+++ userscripts/skip-amazon-prime-checkout-page.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Skip Amazon Prime Checkout Page
+// @description  Skip the page asking for customer to sign up to Amazon Prime checkout
+// @version      1.0.0
+// @author       Karl Horky
+// @namespace    https://www.karlhorky.com/
+// @match        https://www.amazon.de/gp/buy/primeinterstitial/handlers/display.html
+// @grant        none
+// ==/UserScript==
+
+// Click on "Continue and don't gain Amazon Prime benefits"
+document.querySelector('.no-thanks-link > a.prime-nothanks-button').click();
