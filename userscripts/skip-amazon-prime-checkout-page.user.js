ef2354f840c4d63da9afff00991739c6d478504b
Karl Horky <karl.horky@gmail.com>
2021-03-13 18:21:02 +0100

    Fix URL (also match query parameters)

--- userscripts/skip-amazon-prime-checkout-page.user.js
+++ userscripts/skip-amazon-prime-checkout-page.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Skip Amazon Prime Checkout Page
 // @description  Skip the page asking for customer to sign up to Amazon Prime checkout
-// @version      1.0.0
+// @version      1.0.1
 // @author       Karl Horky
 // @namespace    https://www.karlhorky.com/
-// @match        https://www.amazon.de/gp/buy/primeinterstitial/handlers/display.html
+// @match        https://www.amazon.de/gp/buy/primeinterstitial/handlers/display.html*
 // @grant        none
 // ==/UserScript==
 
