656f40204cc98ee5ce27bea188e7f3784c57a1cc
Karl Horky <karl.horky@gmail.com>
2021-04-25 20:02:59 +0200

    Bump version

--- userscripts/highlight-all-occurrences-of-selected-text.user.js
+++ userscripts/highlight-all-occurrences-of-selected-text.user.js
@@ -3,7 +3,7 @@
 // @description  Highlight all occurrences of any text that has been selected on the page
 // @author       James Wilson, Karl Horky
 // @namespace    https://www.karlhorky.com/
-// @version      1.3.0
+// @version      1.3.1
 // @match        https://*/*
 // @match        http://*/*
 // @grant        none
